77880fc1e3cfb38eb28c802fc18b7dfe7db3dcec
CodeShed <Bruce@bennettconsulting.eu>
2021-07-20 11:14:36 +0100

    Add project files.

---
+++ Tests/Test.ModelsProject/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Test.ModelsProject
+{
+    public class Product
+    {
+        public DateTime AvailableFrom { get; set; }
+        public DateTime? AvailableTo { get; set; }
+        public string Name { get; set; }
+        public int ProductId { get; set; }
+        public int ReorderAtItemsInStock { get; set; }
+        public decimal RRP { get; set; }
+    }
+}
---
+++ src/FluentValidation.Document.Rules/Helper.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Reflection;
+
+namespace FluentValidation.Document.Rules
+{
+    public static class Helper
+    {
+        public static class Refelction
+        {
+            private const BindingFlags bindFlags = BindingFlags.Instance | BindingFlags.IgnoreCase | BindingFlags.Public | BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.GetProperty | BindingFlags.Default;
+
+            internal static object RefelctInstanceField(object instance, string fieldName, Type type)
+            {
+                var field = type.GetField(fieldName, bindFlags);
+                return field?.GetValue(instance);
+            }
+
+            internal static object RefelctInstanceProperty(object instance, string propertyName)
+            {
+                string masterPropertyName = propertyName;
+                if (propertyName.Contains("."))
+                {
+                    masterPropertyName = propertyName;
+                    propertyName = masterPropertyName.Split('.')[0];
+                }
+                var fieldValue = instance.GetType().GetProperty(propertyName, bindFlags)?.GetValue(instance);
+                if (masterPropertyName.Contains("."))
+                {
+                    masterPropertyName = masterPropertyName.Substring(propertyName.Length + 1);
+                    fieldValue = RefelctInstanceProperty(fieldValue, masterPropertyName);
+                }
+
+                return fieldValue;
+            }
+
+            internal static object ReflectInstanceField(object instance, string fieldName)
+            {
+                var t = instance.GetType();
+                return RefelctInstanceField(instance, fieldName, t);
+            }
+        }
+    }
+}
